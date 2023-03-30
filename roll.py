2f7253857a1b4332f5db379028d2b1f3cbccea36
yyyaaaccc <j.klawon@gmail.com>
2023-03-30 21:25:34 +0200

    Added handling for additional numeric value to be added to each roll

--- roll.py
+++ roll.py
@@ -3,16 +3,35 @@ import re
 import sys
 
 def roll_dice(dice):
-    # Parse the dice parameter using regular expressions
-    match = re.match(r'^(\d*)d(\d+)$', dice)
+    """
+    Roll the specified dice and return the result as a string.
+
+    The dice parameter should be in the format 'NdS' where N is the number of
+    dice to roll (defaulting to 1 if omitted) and S is the number of sides on
+    each die. For example, 'd20' rolls a 20-sided die once, while '2d6' rolls
+    two 6-sided dice and returns the sum of the results.
+
+    The result string shows each result of the roll concatenated by "+" and
+    summarized as total roll value. For example, '2d6' might return '2d6: 3+5 = 8'.
+    If only one die is rolled, the result string shows only the total roll value.
+
+    If a numeric value is specified after the dice parameter (e.g. '2d6+3'),
+    that value is added to the total roll value. The result string shows the
+    total roll value followed by the added value. For example, '2d6+3' might
+    return '2d6: 3+5+3 = 11'.
+    """
+    # Parse the dice parameter and added value using regular expressions
+    match = re.match(r'^(\d*)d(\d+)(?:\+(\d+))?$', dice)
     if not match:
         raise ValueError('Invalid dice parameter: {}'.format(dice))
     count = int(match.group(1) or 1)
     sides = int(match.group(2))
+    added = int(match.group(3) or 0)
     # Roll the dice and return the result
     rolls = [random.randint(1, sides) for _ in range(count)]
-    total = sum(rolls)
+    total = sum(rolls) + added
     result = '{}: {}'.format(dice, '+'.join(str(r) for r in rolls))
+    result += ' + {}'.format(added) if added else ''
     result += ' = {}'.format(total) if count > 1 else ''
     return result
 
